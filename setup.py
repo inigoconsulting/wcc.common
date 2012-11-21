b19582cb9ff189b2865e2b6db1350786719dc1ec
Izhar Firdaus <izhar@inigo-tech.com>
2012-11-21 11:25:51 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='wcc.common',
+      version=version,
+      description="Common code among all WCC sites",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone wcc',
+      author='Inigo Consulting',
+      author_email='team@inigo-tech.com',
+      url='https://github.com/inigoconsulting/wcc.common',
+      license='gpl',
+      packages=find_packages(),
+      namespace_packages=['wcc'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
