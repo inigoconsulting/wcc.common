bfb19430b0df3f9799a360ef7c3d6bb042f3f559
Izhar Firdaus <izhar@inigo-tech.com>
2013-03-26 17:43:52 +0800

    remove multilingual subscribers as they are now in wcc.multilingual (re #272)

--- wcc/common/subscriber.py
+++
@@ -1,7 +0,0 @@
-from wcc.common.interfaces import ILanguageDependentFieldsManager
-
-def set_default_translation(event):
-    origin = event.object
-    target = event.target
-    ILanguageDependentFieldsManager(origin).copy_fields(target)
-    target.reindexObject()
