91ad10fff8e76cbfc97369bdeba62ce8a1bc80c5
Izhar Firdaus <izhar@inigo-tech.com>
2013-03-27 15:11:37 +0800

    one patch moved to wcc.multilingual

--- wcc/common/patches.py
+++ wcc/common/patches.py
@@ -1,27 +1,6 @@
 from logging import getLogger
 logger = getLogger('wcc.policy.patches')
 
-def _patch_canonicals_cleanup():
-    from plone.multilingual.storage import CanonicalStorage
-    from plone.app.uuid.utils import uuidToObject   
-
-    if getattr(CanonicalStorage, '__wcc_canonical_cleanup_patch', False):
-        return
-    logger.info('Patching with canonical cleanup patch')
-
-    _orig_get_canonicals = CanonicalStorage.get_canonicals
-    def get_canonicals(self):
-        canonicals = _orig_get_canonicals(self)
-        for c in canonicals:
-            obj = uuidToObject(c)
-            if obj is None:
-                self.remove_canonical(c)
-        return _orig_get_canonicals(self)
-    CanonicalStorage.get_canonicals = get_canonicals
-    CanonicalStorage.__wcc_canonical_cleanup_patch = True
-
-_patch_canonicals_cleanup()
-
 def _patch_dont_compress_types():
     from ZPublisher.HTTPResponse import HTTPResponse
     NO_COMPRESSION_TYPES=['application/x-shockwave-flash']
