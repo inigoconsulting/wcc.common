97faddbd921c1d954fd8045c1b27c50ce6aebb69
Izhar Firdaus <izhar@inigo-tech.com>
2013-04-18 15:06:56 +0800

    move homepage purger to wcc.caching (re #316)

--- wcc/common/subscribers.py
+++
@@ -1,8 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-from zope.event import notify
-from z3c.caching.purge import Purge
-
-def purge_homepage(obj, event):
-    catalog = getToolByName(obj, 'portal_catalog')
-    for brain in catalog(portal_type='wcc.homepage.homepage'):
-        notify(Purge(brain.getObject()))
